df93e5717312d2abb8b3cebcadda43b834cee465
SOVEREIGN <isimemeni@gmail.com>
2020-05-27 17:11:37 -0400

    added pages and npm packages

---
+++ strve/src/Pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Jumbotron>
+                <Container>
+                    <h2>strive.</h2>
+                    <p>work from home solutions</p>
+                </Container>
+            </Jumbotron>
+        </div>
+    );
+};
+
+export default HomePage;
